1d2a7672be0c169d1b479c37217286487347cc23
khinswehtike <59813767+khinswehtike@users.noreply.github.com>
2020-04-02 09:56:43 +0630

    fourth commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,140 @@
 import 'package:flutter/material.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+import 'dart:async';
+import 'package:news_mya/Users.dart';
+import 'package:news_mya/Service.dart';
+import 'package:news_mya/Expand.dart';
+void main() => runApp(
+  MaterialApp(
+    home: UserFilterDemo(),
+  )
+);
+ 
+class UserFilterDemo extends StatefulWidget {
+  UserFilterDemo() : super();
+ 
+  final String title = "MyApp";
+ 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  UserFilterDemoState createState() => UserFilterDemoState();
+}
+ 
+class Debouncer {
+  final int milliseconds;
+  VoidCallback action;
+  Timer _timer;
+ 
+  Debouncer({this.milliseconds});
+ 
+  run(VoidCallback action) {
+    if (null != _timer) {
+      _timer.cancel();
+    }
+    _timer = Timer(Duration(milliseconds: milliseconds), action);
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+ 
+class UserFilterDemoState extends State<UserFilterDemo> {
+  // https://jsonplaceholder.typicode.com/users
+ 
+  final _debouncer = Debouncer(milliseconds: 500);
+  List<User> users = List();
+  List<User> filteredUsers = List();
+ 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void initState() {
+    super.initState();
+    Services.getUsers().then((usersFromServer) {
+      setState(() {
+        users = usersFromServer;
+        filteredUsers = users;
+      });
     });
   }
-
+ 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(15.0),
+              hintText: 'Search by name ',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            onChanged: (string) {
+              _debouncer.run(() {
+                setState(() {
+                  filteredUsers = users
+                      .where((u) => (u.name
+                              .toLowerCase()
+                              .contains(string.toLowerCase()) ||
+                          u.email.toLowerCase().contains(string.toLowerCase())))
+                      .toList();
+                });
+              });
+            },
+          ),
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.all(10.0),
+              itemCount: filteredUsers.length,
+              
+              itemBuilder: (BuildContext context, int index) {
+                
+                return InkWell(
+                      onTap: () {
+                       
+                       Navigator.push(
+                         context,MaterialPageRoute(
+                         builder: (context)=>new MyApp()
+                       )
+                       );
+                      },
+               child: Card(
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Image(
+                image: AssetImage('lib/images/jj.png'),
+                fit: BoxFit.fill,
+                width: 50,
+                height: 50,),
+                Expanded(child: ListTile(
+                  title: Text(
+                          filteredUsers[index].name,
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.black,
+                          ),
+                        ),
+                        
+                        
+                        // Text(
+                        //   filteredUsers[index].email.toLowerCase(),
+                        //   style: TextStyle(
+                        //     fontSize: 14.0,
+                        //     color: Colors.grey,
+                        //   ),
+                        // ),
+                )
+                )
+                      ],
+                    ),
+                    
+                  ),
+               )
+                );
+              },
             ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
